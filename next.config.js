a87bb70e73eb92cd866f5d6d5ae3e9e5e30c99ec
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2025-02-06 23:27:08 +0200

    ready to tailwind and css config

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const nextConfig = {
+  // experimental: {
+  //   turbo: {
+  //     rules: {
+  //       "*.css": ["postcss-loader"],
+  //     },
+  //   },
+  // },
+};
+
+module.exports = nextConfig;
